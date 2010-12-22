bd08aa917af385d69e46d0b51315eaaf1fbc51d1
Sam Hon <coffeedaemon@gmail.com>
2010-12-22 16:51:09 +0800

    Rewrote server.js to be a proper javascript class

--- server.js
+++ server.js
@@ -1,53 +1,63 @@
 require.paths.unshift(__dirname + '/lib');
 require.paths.unshift(__dirname);
 
-
-(function main() {
-
-  var http = require('http'),
-      fs = require('fs'),
-      static = require('node-static'),
-      io = require('socket.io'),
-      mongo = require('mongodb'),
-      Hungrybird = require('hungrybird').Hungrybird;
+var Server = function() {
+  var fs = require('fs');
 
   try {
     var configJSON = fs.readFileSync(__dirname + "/config/app.json");
   } catch(e) {
     console.log("File config/app.json not found.  Try: `cp config/app.json.sample config/app.json`");
   }
-  var config = JSON.parse(configJSON.toString());
+  this.config =  JSON.parse(configJSON.toString());
+};
 
-  db = new mongo.Db('hummingbird', new mongo.Server(config.mongo_host, config.mongo_port, {}), {});
+Server.prototype = {
 
-  db.addListener("error", function(error) {
-    console.log("Error connecting to mongo -- perhaps it isn't running?");
-  });
+  run: function() {
 
-  db.open(function(p_db) {
-    var hungrybird = new Hungrybird();
-    hungrybird.init(db, function() {
-      var server = http.createServer(function(req, res) {
-        try {
-          hungrybird.serveRequest(req, res);
-        } catch(e) {
-          hungrybird.handleError(req, res, e);
-        }
-      });
-      server.maxConnections = 3000;
-      server.once('connection', function (stream) {
-        process.setuid(config.node_worker_id);
-      });
-      
-      var nodes = require("multi-node").listen({
-              port         : config.tracking_port, 
-              nodes        : config.node_workers,
-              masterListen : false
-          }, server);
+    var http = require('http'),
+        static = require('node-static'),
+        io = require('socket.io'),
+        mongo = require('mongodb'),
+        Hungrybird = require('hungrybird').Hungrybird;
+
+    var config = this.config;
+
+    db = new mongo.Db(config.mongoDBName, new mongo.Server(config.mongoHost, config.mongoPort, {}), {});
+
+    db.addListener("error", function(error) {
+      console.log("Error connecting to mongo -- perhaps it isn't running?");
     });
 
-  });
+    db.open(function(p_db) {
+      var hungrybird = new Hungrybird();
+      hungrybird.init(db, function() {
+        var server = http.createServer(function(req, res) {
+          try {
+            hungrybird.serveRequest(req, res);
+          } catch(e) {
+            hungrybird.handleError(req, res, e);
+          }
+        });
+        server.maxConnections = config.maxConnections;
+        server.once('connection', function (stream) {
+          process.setuid(config.workerUid);
+        });
 
-})();
+        var nodes = require("multi-node").listen({
+                port         : config.trackingPort,
+                nodes        : config.workerPoolSize,
+                masterListen : false
+            }, server);
+      });
+
+    });
+  }
+
+};
+
+var server = new Server();
+server.run();
 
 
