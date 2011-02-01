61397e6bdcbdfad67c0a77c90a1a32ddb007ac6a
Sam Hon, sam & Winston Teo, winston <pair+sam+winston@viikii.com>
2011-02-01 16:54:07 +0800

    Removing backup after it is uploaded to S3

--- scripts/backup.rb
+++ scripts/backup.rb
@@ -6,22 +6,36 @@ yesterday_timestamp = "#{yesterday.to_time.to_i}000"
 today               = Date.today
 today_timestamp     = "#{today.to_time.to_i}000"
 
-output_dir = "/mnt/dump/#{today.to_s}"
+backup_dir = "/mnt/dump/"
+backup     = "#{backup_dir}#{today.to_s}"
+backup_tgz = "#{today.to_s}.tgz"
 
 # Tables
-cmd = "sudo mongodump --db Hungrybird -c hits -o #{output_dir}  -q '{ timestamp : { $gte : new Date(#{yesterday_timestamp}), $lt: new Date(#{today_timestamp}) } }'"
+cmd = "sudo mongodump --db Hungrybird -c hits -o #{backup}  -q '{ timestamp : { $gte : new Date(#{yesterday_timestamp}), $lt: new Date(#{today_timestamp}) } }'"
 puts cmd
 system(cmd)
 puts "Done with Hits"
 
 # Indexes
-cmd = "sudo mongodump --db Hungrybird -c system.indexes -o #{output_dir}"
+cmd = "sudo mongodump --db Hungrybird -c system.indexes -o #{backup}"
 puts cmd
 system(cmd)
 puts "Done with Indexes"
 
 # Zip
-cmd = "cd /mnt/dump/; sudo tar -czvf #{today.to_s}.tgz #{today.to_s}/"
+cmd = "cd #{backup_dir}; sudo tar -czvf #{backup_tgz} #{today.to_s}/"
 puts cmd
 system(cmd)
 puts "Done with Zipping"
+
+# Upload
+cmd = "cd #{backup_dir}; s3cmd put #{backup_tgz} s3://#{ENV['S3_BUCKET']}/#{backup_tgz}"
+puts cmd
+system(cmd)
+puts "Done with Upload"
+
+# Delete
+cmd = "cd #{backup_dir}; sudo rm -rf #{today.to_s}; sudo rm -rf #{backup_tgz}"
+puts cmd
+# system(cmd)
+puts "Done with Delete"
